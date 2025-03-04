4325806ab4622755688062c731af5d694fea5dbd
yamil perez <yamilperez@new-host.home>
2025-03-04 18:34:29 -0500

    Trigger GitHub Pages Redeploy

--- src/Home.js
+++ src/Home.js
@@ -25,7 +25,7 @@ const Home = () => {
                     playsInline 
                     className={Styles.bannerVideo}
                 >
-                    <source src={samanaVideo} type="video/mp4" />
+                    <source src={samanaVideo ||samanaImg2} type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
                 <div className={Styles.bannerText}>
